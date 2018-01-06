f908ca20eb4e41fd78c9b1d427a7d7b61196ed97
赫英明 <heyingming@M.local>
2018-01-06 17:37:10 +0800

    添加基本数据库

--- sum/hospital/models.py
+++ sum/hospital/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_name = models.CharField(max_length=50)
+    user_password = models.CharField(max_length=16)
+    user_sign = models.BooleanField(default=False)
+
+
+class Hospital(models.Model):
+    location = models.CharField(max_length=50)
+    type = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
+    section = models.CharField(max_length=500)
+    
+
+class Feedback(models.Model):
+    user_id = models.ForeignKey(User) 
+    user_email = models.EmailField()
+    content = models.TextField()
